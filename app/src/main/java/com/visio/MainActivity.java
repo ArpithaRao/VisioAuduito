461b20e2e57916053f92c9f08d17c0184ea7285b
Jayesh Yadav <yadavjayesh@gmail.com>
2016-04-08 16:38:45 -0400

    Nearby Messages Subscription Working

--- app/src/main/java/com/visio/MainActivity.java
+++ app/src/main/java/com/visio/MainActivity.java
@@ -1,18 +1,13 @@
 package com.visio;
 
-import android.app.Fragment;
-import android.content.Context;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,174 +15,78 @@ import com.customlbs.library.Indoors;
 import com.customlbs.library.IndoorsException;
 import com.customlbs.library.IndoorsFactory;
 import com.customlbs.library.IndoorsLocationListener;
-import com.customlbs.library.LocalizationParameters;
 import com.customlbs.library.callbacks.IndoorsServiceCallback;
-import com.customlbs.library.callbacks.LoadingBuildingCallback;
 import com.customlbs.library.callbacks.LoadingBuildingStatus;
 import com.customlbs.library.callbacks.OnlineBuildingCallback;
 import com.customlbs.library.callbacks.ZoneCallback;
 import com.customlbs.library.model.Building;
 import com.customlbs.library.model.Zone;
-import com.customlbs.model.WayPoint;
 import com.customlbs.shared.Coordinate;
 import com.customlbs.surface.library.IndoorsSurfaceFactory;
 import com.customlbs.surface.library.IndoorsSurfaceFragment;
-import com.customlbs.surface.library.UserInteractionListenerImpl;
 
-import java.io.Console;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.EventListener;
 import java.util.List;
-import java.util.PriorityQueue;
-
-import static android.widget.Toast.LENGTH_LONG;
-
-class BuildingAdapter extends ArrayAdapter<Building>{
-
-    static LayoutInflater inflater;
-    Building[] data;
-
-    public BuildingAdapter(Context context, int simple_list_item_1, Building[] listViewArray) {
-        super(context,simple_list_item_1,listViewArray);
-        this.data=listViewArray;
-        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        View view = convertView;
-        if(view==null){
-            view = inflater.inflate(R.layout.building_list_item,parent,false);
-        }
-
-        view.setTag(R.string.tag,data[position]);
-        TextView text=(TextView)view.findViewById(R.id.text);
-        text.setText(data[position].getName());
-
-        return view;
-    }
-
-};
-
-
 
 
 public class MainActivity extends FragmentActivity implements  IndoorsServiceCallback, IndoorsLocationListener {
-
+    public static String TAG = MainActivity.class.getSimpleName();
     public ZoneCallback zonecallback;
     public static OnlineBuildingCallback onlinebuildingcallback;
     public static IndoorsSurfaceFragment indoorsFragment;
     public static IndoorsFactory.Builder indoorsBuilder;
     public static ArrayList<Building> buildingList;
     public static ArrayList<Zone> zonelist;
-
+    public static final String extraName = "BUILDINGID";
+    public final static int REQ_CODE_SPEECH_INPUT = 100;
+    public MainActivity thisObject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        thisObject = this;
         indoorsBuilder = new IndoorsFactory.Builder();
         indoorsBuilder.setContext(this);
         indoorsBuilder.setPassiveServiceCallback(this);
+        indoorsBuilder.setUserInteractionListener(this);
         indoorsBuilder.setApiKey("34420529-47cf-4e4f-a3b6-79f1e0948aab");
-
+        indoorsBuilder.setBuildingId(Long.parseLong(getIntent().getStringExtra(extraName)));
         IndoorsSurfaceFactory.Builder indoorsSurface = new IndoorsSurfaceFactory.Builder();
         indoorsSurface.setIndoorsBuilder(indoorsBuilder);
+
         indoorsFragment = indoorsSurface.build();
 
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(android.R.id.content, indoorsFragment, "indoors");
+        transaction.commit();
 
 
 
 
+    }
 
-        zonecallback=new ZoneCallback() {
-            @Override
-            public void setZones(ArrayList<Zone> arrayList) {
-
-                Log.d("Zone",arrayList.toString());
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQ_CODE_SPEECH_INPUT){
+            if(resultCode==RESULT_OK&&data!=null){
+                ArrayList<String> inputLocationCommand = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                TextView localTextView = (TextView) findViewById(R.id.SpeechOp);
+                Log.d(TAG,inputLocationCommand.get(0));
             }
-        };
-
-        MainActivity.onlinebuildingcallback = new OnlineBuildingCallback() {
-            @Override
-            public void setOnlineBuildings(ArrayList<Building> arrayList) {
-                Building[] listViewArray= new Building[arrayList.size()];
-
-                MainActivity.buildingList =  arrayList;
-
-                ListView listView = (ListView)findViewById(R.id.listView);
-                int i=0;
-
-                for(Building building:buildingList){
-                    listViewArray[i]= building;
-                    i++;
-                }
-
-                //listViewArray= (String[]) buildingList.toArray();
-
-                listView.setAdapter(new BuildingAdapter(MainActivity.this, android.R.layout.simple_list_item_1, listViewArray));
-
-                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        final Indoors temp = MainActivity.indoorsFragment.getIndoors();
-                        Building building =(Building)view.getTag(R.string.tag);
-                        Log.d("building",building.toString());
-                        temp.getBuilding(building, new LoadingBuildingCallback() {
-                            @Override
-                            public void loadingBuilding(LoadingBuildingStatus loadingBuildingStatus) {
-
-                            }
-
-                            @Override
-                            public void buildingLoaded(Building building) {
-                                Log.d("Building","Loaded");
-                                temp.getZones(building, new ZoneCallback() {
-                                    @Override
-                                    public void setZones(ArrayList<Zone> arrayList) {
-                                        Log.d("this", "find");
-                                    }
-                                });
-                            }
-
-                            @Override
-                            public void buildingLoadingCanceled() {
-
-                            }
-
-                            @Override
-                            public void onError(IndoorsException e) {
-                                e.printStackTrace();
-                            }
-                        });
-                    }
-                });
-
-            }
-        };
-
+        }
     }
 
     @Override
     public void connected() {
         Indoors localIndoors=MainActivity.indoorsFragment.getIndoors();
 
-        localIndoors.getOnlineBuildings(onlinebuildingcallback);
-
-
-
-
-
-
-        //Log.d("MyActivity",);
-
-
-    }
+        Log.d("MainActivity",(indoorsFragment.getZones().toString()));
+}
 
     @Override
     public void onError (IndoorsException e){
-
+        Log.d(TAG,"OnError");
     }
 
     public MainActivity() {
@@ -196,11 +95,19 @@ public class MainActivity extends FragmentActivity implements  IndoorsServiceCal
 
     @Override
     public void loadingBuilding(LoadingBuildingStatus loadingBuildingStatus) {
-
     }
 
     @Override
     public void buildingLoaded(Building building) {
+
+        indoorsFragment.getIndoors().getZones(building, new ZoneCallback() {
+            @Override
+            public void setZones(ArrayList<Zone> arrayList) {
+                Log.d(TAG,arrayList.toString());
+                (new VoiceCommandInput(thisObject)).takeSpeechInput();
+            }
+        });
+
 
     }
 
@@ -233,4 +140,23 @@ public class MainActivity extends FragmentActivity implements  IndoorsServiceCal
     public void buildingLoadingCanceled() {
 
     }
-};
+}
+
+class VoiceCommandInput{
+    private Activity callingActivity;
+
+    public VoiceCommandInput(Activity callingActivity) {
+        this.callingActivity = callingActivity;
+    }
+
+    public void takeSpeechInput(){
+        Intent intentHandle = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intentHandle.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intentHandle.putExtra(RecognizerIntent.EXTRA_PROMPT,"Where would you like to go?");
+        try{
+            callingActivity.startActivityForResult(intentHandle,MainActivity.REQ_CODE_SPEECH_INPUT);
+        }catch(ActivityNotFoundException e){
+            Toast.makeText(callingActivity,"Speech Input not supported on this device",Toast.LENGTH_LONG).show();
+        }
+    }
+}
